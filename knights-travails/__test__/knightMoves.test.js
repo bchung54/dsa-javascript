fca6071417b3da0c0a7028e88d62ea7f577cdc09
bchung-desktop <brianchung54@gmail.com>
2022-08-18 08:21:46 -0700

    Clean folder and knightMoves test
    Refactor knighMoves

--- knights-travails/__test__/knightMoves.test.js
+++ knights-travails/__test__/knightMoves.test.js
@@ -1,18 +1,5 @@
 import knightMoves from '../knightMoves';
 
-/* it('should return [[6,5], [5,6]] for adjacent vertices of [7,7]', () => {
-    expect(knightMoves([0, 1], [2, 2]).findAdjacentVertices([7, 7])).toEqual([
-			[6, 5],
-			[5, 6]
-		]);
-});
-
-it('should return an array of length 63 for unvisited nodes when initialized', () => {
-    const unvisited = knightMoves([0, 1], [2, 2]).unvisited;
-    console.log(unvisited)
-	expect(unvisited.length).toBe(63);
-}); */
-
 it('should return correct array for knightMoves function', () => {
 	expect(knightMoves([0, 0], [1, 2])).toEqual([
 		[0, 0],
